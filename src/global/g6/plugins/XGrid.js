9e44238a3f8d966fd8e3b40dc5b79658a2398d59
Michael Alemu <michael.alemu@gmail.com>
2020-05-01 12:49:31 +0200

    update

--- src/global/g6/plugins/XGrid.js
+++ src/global/g6/plugins/XGrid.js
@@ -6,17 +6,32 @@
  * 扩展G6自带Grid插件
  */
 
-import Grid from '@antv/g6/build/grid'
-import modifyCSS from '@antv/util/lib/dom/modify-css'
+import G6 from "@antv/G6";
+import modifyCSS from "@antv/util/lib/dom/modify-css";
+
+const Grid = new G6.Grid();
 
 export default class XGrid extends Grid {
-  updateGrid (e) {
-    const gridContainer = this.get('gridContainer')
-    const matrix = e.matrix
+  updateGrid(e) {
+    const gridContainer = this.get("gridContainer");
+    const matrix = e.matrix;
     // 矩阵变换
-    const transform = 'matrix(' + matrix[0] + ',' + matrix[1] + ',' + matrix[3] + ',' + matrix[4] + ',' + matrix[6] + ',' + matrix[7] + ')'
+    const transform =
+      "matrix(" +
+      matrix[0] +
+      "," +
+      matrix[1] +
+      "," +
+      matrix[3] +
+      "," +
+      matrix[4] +
+      "," +
+      matrix[6] +
+      "," +
+      matrix[7] +
+      ")";
     modifyCSS(gridContainer, {
       transform
-    })
+    });
   }
 }
