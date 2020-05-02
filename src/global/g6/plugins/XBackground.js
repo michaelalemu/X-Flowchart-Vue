84c40be538900640c10e0df705d3cf931d2abcee
Michael Alemu <michael.alemu@gmail.com>
2020-05-02 10:33:31 +0200

    Revert "update"
    This reverts commit 9e44238a3f8d966fd8e3b40dc5b79658a2398d59.

--- src/global/g6/plugins/XBackground.js
+++ src/global/g6/plugins/XBackground.js
@@ -5,67 +5,63 @@
  */
 
 // import Base from '@antv/g6/plugins/base'
-import Grid from "@antv/g6/build/grid";
-import createDOM from "@antv/util/lib/dom/create-dom";
-import modifyCSS from "@antv/util/lib/dom/modify-css";
-
-import G6 from "@antv/G6";
-
-const Grid = new G6.Grid();
+import Grid from '@antv/g6/build/grid'
+import createDOM from '@antv/util/lib/dom/create-dom'
+import modifyCSS from '@antv/util/lib/dom/modify-css'
 
 export default class XBackground extends Grid {
-  init() {
-    let _t = this;
-    const graph = _t.get("graph");
-    const graphContainer = graph.get("container");
-    const canvas = graph.get("canvas").get("el");
+  init () {
+    let _t = this
+    const graph = _t.get('graph')
+    const graphContainer = graph.get('container')
+    const canvas = graph.get('canvas').get('el')
     const backgroundContainer = createDOM(
       '<div class="x-background" style="position: absolute; left: 0; top:0; right:0; bottom:0; overflow: hidden; z-index: -999;"></div>'
-    );
+    )
     const imgDom = createDOM(
       '<img class="x-background-img" style="width: 100%; height: 100%; visibility: hidden;"></img>'
-    );
-    backgroundContainer.appendChild(imgDom);
-    graphContainer.insertBefore(backgroundContainer, canvas);
-    _t.set("container", backgroundContainer);
-    _t.set("imgDom", imgDom);
+    )
+    backgroundContainer.appendChild(imgDom)
+    graphContainer.insertBefore(backgroundContainer, canvas)
+    _t.set('container', backgroundContainer)
+    _t.set('imgDom', imgDom)
   }
-  getEvents() {
+  getEvents () {
     return {
-      "background:reset": "resetBackground",
-      "background:update": "updateBackground"
-    };
+      'background:reset': 'resetBackground',
+      'background:update': 'updateBackground'
+    }
   }
   // 重置背景
-  resetBackground(e) {
-    let _t = this;
-    let imgDom = _t.get("imgDom");
+  resetBackground (e) {
+    let _t = this
+    let imgDom = _t.get('imgDom')
     if (imgDom) {
-      imgDom.src = "";
+      imgDom.src = ''
       modifyCSS(imgDom, {
-        visibility: "hidden"
-      });
+        visibility: 'hidden'
+      })
     }
   }
-  updateBackground(data) {
-    let _t = this;
-    let imgDom = _t.get("imgDom");
+  updateBackground (data) {
+    let _t = this
+    let imgDom = _t.get('imgDom')
     if (imgDom) {
-      imgDom.src = data;
+      imgDom.src = data
       modifyCSS(imgDom, {
-        visibility: "visible"
-      });
+        visibility: 'visible'
+      })
     }
   }
-  updateGrid() {}
-  getContainer() {
-    return this.get("container");
+  updateGrid () {}
+  getContainer () {
+    return this.get('container')
   }
-  destroy() {
-    let _t = this;
-    const graph = _t.get("graph");
-    const graphContainer = graph.get("container");
-    const backgroundContainer = _t.get("container");
-    graphContainer.removeChild(backgroundContainer);
+  destroy () {
+    let _t = this
+    const graph = _t.get('graph')
+    const graphContainer = graph.get('container')
+    const backgroundContainer = _t.get('container')
+    graphContainer.removeChild(backgroundContainer)
   }
 }
